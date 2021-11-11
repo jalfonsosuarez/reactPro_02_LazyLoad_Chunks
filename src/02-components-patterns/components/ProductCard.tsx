9692feffe7aee673285e082a00bbf44db026687b
= <=>
2021-11-11 19:54:50 +0100

    ProductCard

--- src/02-components-patterns/components/ProductCard.tsx
+++ src/02-components-patterns/components/ProductCard.tsx
@@ -1,9 +1,33 @@
-
+import { useState } from 'react';
+import styles from '../styles/styles.module.css';
+import noImage from '../assets/no-image.jpg';
 
 export const ProductCard = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    const increaseBy = ( value: number ) => {
+        setCounter( prev => Math.max( prev + value, 0) );
+    };
+
     return (
-        <div>
-            <h1>Product Card</h1>
+        <div className={ styles.productCard }>
+            <img className={ styles.productImg } src="./coffee-mug.png" alt="Coffee-mug" />
+            {/* <img className={ styles.productImg } src={ noImage } alt="Sin imagen" /> */}
+
+            <span className={ styles.productDescription }>Cofee Mug</span>
+
+            <div className={ styles.buttonsContainer }>
+                <button 
+                    className={ styles.buttonMinus }
+                    onClick={ () => increaseBy( -1 ) }
+                > - </button>
+                <div className={ styles.countLabel }> { counter } </div>
+                <button 
+                    className={ styles.buttonAdd }
+                    onClick={ () => increaseBy( 1 ) }
+                > + </button>
+            </div>
         </div>
     )
 }
