736f3cee2828f651f4772b3a94114bc2e19e2deb
= <=>
2021-11-12 19:15:31 +0100

    Product component DIY ready.

--- src/02-components-patterns/components/ProductCard.tsx
+++ src/02-components-patterns/components/ProductCard.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react';
+import { useProduct } from '../hooks/useProduct';
+
 import styles from '../styles/styles.module.css';
-import noImage from '../assets/no-image.jpg';
 
-export const ProductCard = () => {
+import { createContext  } from 'react';
+import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces';
 
-    const [counter, setCounter] = useState(0);
 
-    const increaseBy = ( value: number ) => {
-        setCounter( prev => Math.max( prev + value, 0) );
-    };
+export const ProductContext = createContext( {} as ProductContextProps );
+const { Provider } = ProductContext;
+
+export const ProductCard = ( { children, product }: ProductCardProps ) => {
+
+    const { counter, increaseBy } = useProduct()
 
     return (
-        <div className={ styles.productCard }>
-            <img className={ styles.productImg } src="./coffee-mug.png" alt="Coffee-mug" />
-            {/* <img className={ styles.productImg } src={ noImage } alt="Sin imagen" /> */}
 
-            <span className={ styles.productDescription }>Cofee Mug</span>
+        <Provider value={{
+            counter,
+            increaseBy,
+            product
+        }}>
 
-            <div className={ styles.buttonsContainer }>
-                <button 
-                    className={ styles.buttonMinus }
-                    onClick={ () => increaseBy( -1 ) }
-                > - </button>
-                <div className={ styles.countLabel }> { counter } </div>
-                <button 
-                    className={ styles.buttonAdd }
-                    onClick={ () => increaseBy( 1 ) }
-                > + </button>
+            <div className={ styles.productCard }>
+                { children }
             </div>
-        </div>
+
+        </Provider>
     )
 }
--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,39 @@
-import { ProductCard } from "../components/ProductCard"
+import { ProductCard } from "../components";
 
+
+const product = {
+    id: '1',
+    title: 'Coffee Mug - Card',
+    img: './coffee-mug.png',
+};
 
 export const ShoppingPage = () => {
     return (
         <div>
             <h1>Shopping Page</h1>
             <hr />
-            <ProductCard />
+
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap'
+            }}>
+
+                {/* <ProductCard product = { product }>
+                    <ProductCard.Image />
+                    <ProductCard.Title title= { '' } />
+                    <ProductCard.Buttons increaseBy={function (value: number): void {
+                                        throw new Error("Function not implemented.");
+                                    }} counter={0} />
+                </ProductCard> */}
+
+                <ProductCard product = { product }>
+                    <ProductCard.Image />
+                    <ProductCard.Title />
+                    <ProductCard.Buttons />
+                </ProductCard>
+
+            </div>
         </div>
     )
 }
