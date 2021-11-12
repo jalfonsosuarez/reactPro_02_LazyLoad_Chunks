722bb5a3bd02417026e9654a446b05c8bb38cfac
= <=>
2021-11-12 19:18:22 +0100

    Bug unused imports.

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,9 +6,6 @@ import {
     NavLink
 } from 'react-router-dom';
 
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
-
-
 import logo from '../logo.svg';
 
 export const Navigation = () => {
